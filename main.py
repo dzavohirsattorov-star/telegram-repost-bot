da1ca18fdf298536b362fc8d1d82391a58dc64bd
dzavohirsattorov-star <dzavohirsattorov@gmail.com>
2026-06-06 11:33:58 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-from telethon import TelegramClient, events
-import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes
+import asyncio
 
-API_ID = 39992891
-API_HASH = "f8379d46a5f921ad5040a548cfec0839"
-SOURCE = "futbol_fudbol_sport_tv_gollar"
-TARGET = "jaxon_chemp"
+BOT_TOKEN = "8955269705:AAE-EG8l1be_6j587V1aTLiARkZWoaupsz0"
+SOURCE = "@futbol_fudbol_sport_tv_gollar"
+TARGET = "@jaxon_chemp"
 
-client = TelegramClient('bot', API_ID, API_HASH)
+async def check_and_forward(app):
+    last_id = 0
+    while True:
+        try:
+            updates = await app.bot.get_updates(offset=last_id)
+            channel = await app.bot.get_chat(SOURCE)
+            messages = await app.bot.get_chat(channel.id)
+        except:
+            pass
+        await asyncio.sleep(60)
 
-@client.on(events.NewMessage(chats=SOURCE))
-async def handler(event):
-    await client.forward_messages(TARGET, event.message)
-
-client.start()
-client.run_until_disconnected()
+if __name__ == "__main__":
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    asyncio.run(check_and_forward(app))
